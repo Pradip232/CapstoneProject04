a7117c91872ed5ab69d1612eb1d8fedc270231de
Pradip Kolhe <pradipk3@birlasoft.com>
2022-09-27 09:20:26 +0530

    Added EmailSenderService

---
+++ eTraveller/Services/MyEmailSenderService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace eTraveller.Services
+{
+    public class MyEmailSenderService : IEmailSender
+    {
+        private readonly ILogger<MyEmailSenderService> _logger;
+
+        public MyEmailSenderService(ILogger<MyEmailSenderService> logger)
+        {
+            _logger = logger;
+        }
+        #region IEmailSender Member
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            _logger.LogInformation($"Email sent to: {email}, Content: {htmlMessage}");
+            return Task.CompletedTask;
+        
+        }
+        #endregion
+    }
+}
